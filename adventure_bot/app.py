8fad7d657b05255ea5d85232f2828a7fd7839961
bn-hull <bhull@nevada.unr.edu>
2021-04-27 04:33:00 -0700

    deleting example code

--- adventure_bot/app.py
+++
@@ -1,3 +0,0 @@
-# simple function for getting set up. returns a var
-def sample_func(var):
-    return var
--- tests/test_app.py
+++
@@ -1,7 +0,0 @@
-from adventure_bot import app
-
-
-class TestSampleMethods:
-    def test_(self):
-        print("passed")
-        assert 1 == 1
